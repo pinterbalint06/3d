97b79da45ac13e3e2b6ecbd27af563cdda444ad7
Pinter Balint <pinter.balint06@gmail.com>
2026-01-03 23:26:41 +0100

    Canvas bemenetek külön osztállyal kezelve
    - CanvasInput class kezeli a forgatást és nagyítást
    - test-terrainban is a CanvasInput használata

--- frontend/js/test-equirectangular.js
+++ frontend/js/test-equirectangular.js
@@ -1,8 +1,7 @@
+import { CanvasInput } from './CanvasInput.js';
+
 // kamera tulajdonsagai
 let fokuszTavolsag = 18.0; // mm focalLength
-const minfokuszTavolsag = 10.5;
-const maxfokuszTavolsag = 150.0;
-const zoomSpeed = 0.05;
 const filmSzel = 25.4;
 const filmMag = 25.4;
 const jsCanvasSzelesseg = 1000;
@@ -13,125 +12,37 @@ const n = 0.1;
 const f = 1000;
 const canvasId = "canvas";
 
+function initModule() {
+    console.log("module betoltve");
+    Module.init(256, fokuszTavolsag, filmSzel, filmMag, jsCanvasSzelesseg, jsCanvasMagassag, n, f);
+    Module.setShadingTexture();
+    imgFromUrl("../imgs/cathedral.jpg");
+    Module.startRenderingLoop();
+
+    let canvas = document.getElementById(canvasId);
+    let inputControls = new CanvasInput(canvas, {
+        focalLength: fokuszTavolsag,
+        onRotate: (x, y) => {
+            xyForgas(x, y);
+        },
+        onZoom: (ujFokuszTavolsag) => {
+            Module.changeFocalLength(ujFokuszTavolsag);
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
     let canvas = document.getElementById(canvasId);
     canvas.width = jsCanvasSzelesseg;
     canvas.height = jsCanvasMagassag;
 
-    let utolsoX = 0;
-    let utolsoY = 0;
-    let prevDiff = 0;
-    const sensitivity = 0.2;
-    let pointers = [];
-
     canvas.style.cursor = "grab";
 
-    Module.onRuntimeInitialized = function () {
-        console.log("module betoltve");
-        Module.init(256, fokuszTavolsag, filmSzel, filmMag, jsCanvasSzelesseg, jsCanvasMagassag, n, f);
-        Module.setShadingTexture();
-        imgFromUrl("../imgs/cathedral.jpg");
-        Module.startRenderingLoop();
-
-        canvas.addEventListener('pointerdown', (e) => {
-            if (pointers.length < 2) {
-                pointers.push(e);
-                if (pointers.length == 1) {
-                    utolsoX = e.clientX;
-                    utolsoY = e.clientY;
-                } else {
-                    if (pointers.length == 2) {
-                        // két pont távolsága
-                        prevDiff = Math.sqrt(
-                            Math.pow(pointers[0].clientX - pointers[1].clientX, 2) +
-                            Math.pow(pointers[0].clientY - pointers[1].clientY, 2)
-                        );
-                    }
-                }
-            }
-
-            canvas.style.cursor = pointers.length == 1 ? "grabbing" : "grab";
-        });
-
-        canvas.addEventListener('pointerup', (e) => {
-            let i = 0;
-            while (i < pointers.length && pointers[i].pointerId != e.pointerId) {
-                i++;
-            }
-            if (i < pointers.length) {
-                pointers.splice(i, 1);
-            }
-
-            if (pointers.length == 1) {
-                prevDiff = 0;
-                utolsoX = pointers[0].clientX;
-                utolsoY = pointers[0].clientY;
-            }
-
-            if (pointers.length == 0) {
-                canvas.style.cursor = "grab";
-            }
-        });
-
-        canvas.addEventListener('pointermove', (e) => {
-            let i = 0;
-            while (i < pointers.length && pointers[i].pointerId != e.pointerId) {
-                i++;
-            }
-            if (i < pointers.length) {
-                pointers[i] = e;
-            }
-            if (pointers.length == 1) {
-
-                let dX = e.clientX - utolsoX;
-                let dY = e.clientY - utolsoY;
-
-                utolsoX = e.clientX;
-                utolsoY = e.clientY;
-
-                // jobbra huzza balra mozogjon -> invertalni kell
-                const rotX = -dY * sensitivity * (minfokuszTavolsag / fokuszTavolsag);
-                const rotY = -dX * sensitivity * (minfokuszTavolsag / fokuszTavolsag);
-
-                xyForgas(rotX, rotY);
-            } else {
-                if (pointers.length == 2) {
-                    let currDiff = Math.sqrt(
-                        Math.pow(pointers[0].clientX - pointers[1].clientX, 2) +
-                        Math.pow(pointers[0].clientY - pointers[1].clientY, 2)
-                    );
-                    let valtozas = (currDiff - prevDiff) * sensitivity;
-                    fokuszTavolsag += valtozas;
-
-                    if (fokuszTavolsag < minfokuszTavolsag) {
-                        fokuszTavolsag = minfokuszTavolsag;
-                    };
-                    if (fokuszTavolsag > maxfokuszTavolsag) {
-                        fokuszTavolsag = maxfokuszTavolsag;
-                    };
-
-                    prevDiff = currDiff;
-                    Module.changeFocalLength(fokuszTavolsag);
-                }
-            }
-        });
-
-        canvas.addEventListener('wheel', (e) => {
-            e.preventDefault();
-
-            const d = e.deltaY * -zoomSpeed;
-            fokuszTavolsag += d;
-
-            if (fokuszTavolsag < minfokuszTavolsag) {
-                fokuszTavolsag = minfokuszTavolsag;
-            };
-            if (fokuszTavolsag > maxfokuszTavolsag) {
-                fokuszTavolsag = maxfokuszTavolsag;
-            };
-
-            Module.changeFocalLength(fokuszTavolsag);
-        });
-    };
+    if (Module.calledRun) {
+        initModule();
+    } else {
+        Module.onRuntimeInitialized = initModule;
+    }
 });
 
 function ujUrlbol() {
@@ -179,3 +90,7 @@ function imgFromUrl(url) {
 function xyForgas(xszoggel, yszoggel) {
     Module.xyForog(xszoggel * (Math.PI / 180), yszoggel * (Math.PI / 180));
 }
+
+window.xyForgas = xyForgas;
+window.ujUrlbol = ujUrlbol;
+window.ujElsmitas = ujElsmitas;
